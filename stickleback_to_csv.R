e55ff8fe7daede8bf219b25a7f28ee9e61c14dcf
Max Czapanskiy <maxczap@stanford.edu>
2022-06-06 09:43:17 -0700

    Add event data and remove CSVs from repo

--- stickleback_to_csv.R
+++ stickleback_to_csv.R
@@ -5,15 +5,36 @@ c(lunge_sensors, lunge_events) %<-% load_lunges()
 sensors_df <- as.data.frame(lunge_sensors)
 events_df <- as.data.frame(lunge_events)
 
-write_deployment <- function(df) {
+dir.create("data")
+dir.create("data/sensors")
+dir.create("data/events")
+
+write_sensors <- function(df) {
   deployid <- df$deployid[1]
-  file_path <- sprintf("csv/%s.csv", deployid)
+  file_path <- sprintf("data/sensors/%s.csv", deployid)
   df %>%
     select(-deployid) %>%
+    rename(timestamp_utc = datetime) %>%
+    write_csv(file_path)
+}
+
+write_events <- function(df) {
+  deployid <- df$deployid[1]
+  file_path <- sprintf("data/events/%s.csv", deployid)
+  df %>%
+    select(-deployid) %>%
+    rename(lunge = datetime) %>%
     write_csv(file_path)
 }
 
 sensors_df %>%
   group_split(deployid) %>%
-  walk(write_deployment)
+  walk(write_sensors)
+
+events_df %>%
+  group_split(deployid) %>%
+  walk(write_events)
+
+zip("bluewhaledata.zip", "data/")
+
 
