93e74e1d1bd783abb7f34d6ac8fa89113194f46c
Max Czapanskiy <maxczap@stanford.edu>
2022-06-06 17:59:41 -0700

    Tidy data processing script

--- stickleback_to_csv.R
+++ stickleback_to_csv.R
@@ -1,14 +1,19 @@
 library(rstickleback)
 library(tidyverse)
-c(lunge_sensors, lunge_events) %<-% load_lunges()
 
+# Read sample data and convert to tabular form
+c(lunge_sensors, lunge_events) %<-% load_lunges()
 sensors_df <- as.data.frame(lunge_sensors)
 events_df <- as.data.frame(lunge_events)
 
+# Create folder structure, removing prior artifacts if applicable
+if (file.exists("bluewhaledata.zip")) file.remove("bluewhaledata.zip")
+if (dir.exists("data")) unlink("data", recursive = TRUE)
 dir.create("data")
 dir.create("data/sensors")
 dir.create("data/events")
 
+# Write sensor and event data as CSVs
 write_sensors <- function(df) {
   deployid <- df$deployid[1]
   file_path <- sprintf("data/sensors/%s_sensors.csv", deployid)
@@ -17,7 +22,6 @@ write_sensors <- function(df) {
     rename(timestamp_utc = datetime) %>%
     write_csv(file_path)
 }
-
 write_events <- function(df) {
   deployid <- df$deployid[1]
   file_path <- sprintf("data/events/%s_events.csv", deployid)
@@ -26,15 +30,13 @@ write_events <- function(df) {
     rename(lunge = datetime) %>%
     write_csv(file_path)
 }
-
 sensors_df %>%
   group_split(deployid) %>%
   walk(write_sensors)
-
 events_df %>%
   group_split(deployid) %>%
   walk(write_events)
 
+# Zip data and remove intermediary files
 zip("bluewhaledata.zip", "data/")
-
 unlink("data", recursive = TRUE)
